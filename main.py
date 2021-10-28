b948b856c30d5070b25e365628bb919978e5436d
Unerasable <shayanariz99@gmail.com>
2021-10-28 20:52:29 +0530

    Image2Sktech(v.0.1)

---
+++ main.py
@@ -0,0 +1,12 @@
+import cv2
+
+image_input = input("The location of the image: ")
+image = cv2.imread(image_input)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+inverted_image = 255 - gray_image
+blurred = cv2.GaussianBlur(inverted_image, (21, 21), 0)
+inverted_blurred = 255 - blurred
+pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=256.0)
+cv2.imshow("Original Image", image)
+cv2.imshow("Pencil Sketch of the Image", pencil_sketch)
+cv2.waitKey(0)
