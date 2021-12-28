54e4d03a209bfcb9e231af8d994a431990f961ef
Unerasable <shayanariz99@gmail.com>
2021-12-28 11:38:06 +0530

    update

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import cv2
-image_input = input("The location of the image: ")
-image = cv2.imread(image_input)
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-inverted_image = 255 - gray_image
-blurred = cv2.GaussianBlur(inverted_image, (21, 21), 0)
-inverted_blurred = 255 - blurred
-pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=256.0)
-cv2.imshow("Original Image", image)
-cv2.imshow("Pencil Sketch of the Image", pencil_sketch)
-cv2.waitKey(0)
+img = input("Location of the image: ")
+image = cv2.imread(img)  # loads an image from the specified file
+# convert an image from one color space to another
+grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+invert = cv2.bitwise_not(grey_img)  # helps in masking of the image
+# sharp edges in images are smoothed while minimizing too much blurring
+blur = cv2.GaussianBlur(invert, (21, 21), 0)
+invertedblur = cv2.bitwise_not(blur)
+sketch = cv2.divide(grey_img, invertedblur, scale=256.0)
+cv2.imwrite("sketch.png", sketch)  # converted image is saved as mentioned name
